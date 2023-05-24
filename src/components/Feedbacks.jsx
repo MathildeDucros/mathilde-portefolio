e8344909ebc7aecd928ad57b9fdd1de47ff33da8
Mathilde <ducros.mathilde@gmail.com>
2023-05-24 17:52:37 +0200

    modale

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -11,7 +11,7 @@ const FeedbackCard = ({
   name,
   designation,
   company,
-  image,
+  // image,
 }) => (
   <motion.div
     variants={fadeIn("", "spring", index * 0.5, 0.75)}
@@ -44,6 +44,7 @@ const FeedbackCard = ({
 
 const Feedbacks = () => {
   const [testimonials, setTestimonials] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
     fetch("https://portfolio-testiminials.onrender.com/")
@@ -64,6 +65,10 @@ const Feedbacks = () => {
     />
   ));
 
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
   return (
     <div className={`mt-12 bg-black-100 rounded-[20px]`}>
       <div
@@ -76,6 +81,127 @@ const Feedbacks = () => {
       </div>
       <div className={`-mt-20 pb-14 ${styles.paddingX} flex flex-wrap gap-7`}>
         {renderedTestimonials}
+        <motion.button
+          onClick={openModal}
+          className="bg-tertiary text-white px-4 py-2 rounded-full hover:bg-opacity-90 transition-all duration-200 ease-in-out"
+        >
+          Ajouter un témoignage
+        </motion.button>
+      </div>
+      {modalOpen && <FeedbackModal closeModal={() => setModalOpen(false)} />}
+    </div>
+  );
+};
+
+const FeedbackModal = ({ closeModal }) => {
+  const [testimonial, setTestimonial] = useState("");
+  const [name, setName] = useState("");
+  const [designation, setDesignation] = useState("");
+  const [company, setCompany] = useState("");
+
+  const handleSoumettre = () => {
+    fetch("https://portfolio-testiminials.onrender.com/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        commentaire: testimonial,
+        company: company,
+        designation: designation,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result) {
+          closeModal();
+
+          // Traitement des données de réponse si nécessaire
+          console.log("Réponse de la base de données :", data);
+        }
+      })
+      .catch((error) => {
+        // Gestion des erreurs
+        console.log("Erreur lors de l'enregistrement des données :", error);
+      });
+  };
+
+  return (
+    <div className="fixed inset-0 flex justify-center items-center bg-gray-900 bg-opacity-75 z-50">
+      <div className="bg-tertiary p-8 rounded-lg">
+        <h2 className="text-xl font-bold mb-4 text-secondary">
+          Ajouter un témoignage
+        </h2>
+        <form>
+          <div className="mb-4">
+            <label htmlFor="testimonial" className="block mb-2">
+              Commentaire
+            </label>
+            <textarea
+              id="testimonial"
+              value={testimonial}
+              onChange={(e) => setTestimonial(e.target.value)}
+              className="w-full p-2 border rounded"
+              required
+            ></textarea>
+          </div>
+          <div className="mb-4">
+            <label htmlFor="name" className="block mb-2">
+              Nom
+            </label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="designation" className="block mb-2">
+              Désignation
+            </label>
+            <input
+              type="text"
+              id="designation"
+              value={designation}
+              onChange={(e) => setDesignation(e.target.value)}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="company" className="block mb-2">
+              Entreprise
+            </label>
+            <input
+              type="text"
+              id="company"
+              value={company}
+              onChange={(e) => setCompany(e.target.value)}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+          <div className="flex justify-end">
+            <button
+              type="button"
+              onClick={closeModal}
+              className="mr-2 px-4 py-2 text-gray-600 rounded"
+            >
+              Annuler
+            </button>
+            <button
+              type="submit"
+              className="px-4 py-2 bg-tertiary text-secondary rounded"
+              onClick={handleSoumettre}
+            >
+              Soumettre
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
